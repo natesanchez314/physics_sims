9440ebf12225080b6e5642f7829c3dd194a0b235
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2023-06-22 12:06:18 -0500

    start new rust project

---
+++ target/debug/deps/physics_sims.d
@@ -0,0 +1,5 @@
+C:\Users\nsanch17\Documents\GitHub\physics_sims\target\debug\deps\physics_sims.exe: src\main.rs
+
+C:\Users\nsanch17\Documents\GitHub\physics_sims\target\debug\deps\physics_sims.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/physics_sims.d
@@ -0,0 +1,1 @@
+C:\Users\nsanch17\Documents\GitHub\physics_sims\target\debug\physics_sims.exe: C:\Users\nsanch17\Documents\GitHub\physics_sims\src\main.rs
